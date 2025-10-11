8c4a3c31dc1aaaafaa4808ebae228404cbb199d6
Norberto Lopes <nlopes.ml@gmail.com>
2025-10-11 17:48:59 +0200

    docs(core): improve the docs on Doctype, SafeMode, Source

--- acdc-core/src/lib.rs
+++ acdc-core/src/lib.rs
@@ -2,13 +2,34 @@ use std::path::PathBuf;
 
 use clap::ValueEnum;
 
-/// document type to use when converting document
+/// Document type to use when converting document.
 #[derive(Debug, Clone, ValueEnum, Default)]
 pub enum Doctype {
+    /// The default doctype. In DocBook, this includes the appendix, abstract,
+    /// bibliography, glossary, and index sections. Unless you are making a book or a man
+    /// page, you don’t need to worry about the doctype. The default will suffice.
     #[default]
     Article,
+
+    /// Builds on the article doctype with the additional ability to use a top-level title
+    /// as part titles, includes the appendix, dedication, preface, bibliography,
+    /// glossary, index, and colophon. There’s also the concept of a multi-part book, but
+    /// the distinction from a regular book is determined by the content. A book only has
+    /// chapters and special sections, whereas a multi-part book is divided by parts that
+    /// each contain one or more chapters or special sections.
     Book,
+
+    /// Used for producing a roff or HTML-formatted manual page (man page) for Unix and
+    /// Unix-like operating systems. This doctype instructs the parser to recognize a
+    /// special document header and section naming conventions for organizing the AsciiDoc
+    /// content as a man page. See Generate Manual Pages from AsciiDoc for details on how
+    /// structure a man page using AsciiDoc and generate it using Asciidoctor.
     Manpage,
+
+    /// There may be cases when you only want to apply inline AsciiDoc formatting to input
+    /// text without wrapping it in a block element. For example, in the Asciidoclet
+    /// project (AsciiDoc in Javadoc), only the inline formatting is needed for the text
+    /// in Javadoc tags.
     Inline,
 }
 
@@ -23,20 +44,65 @@ impl std::fmt::Display for Doctype {
     }
 }
 
-/// safe mode to use when converting document
+/// Safe mode to use when processing the document. This follows from what is described in
+/// https://docs.asciidoctor.org/asciidoctor/latest/safe-modes/ and is intended to provide
+/// similar functionality as Asciidoctor.
 #[derive(Debug, Clone, ValueEnum, Default, PartialOrd, PartialEq)]
 pub enum SafeMode {
+    /// The UNSAFE safe mode level disables all security measures.
     #[default]
     Unsafe = 0,
+
+    /// The SAFE safe mode level prevents access to files which reside outside of the
+    /// parent directory of the source file. Include directives (include::[]) are enabled,
+    /// but paths to include files must be within the parent directory. This mode allows
+    /// assets (such as the stylesheet) to be embedded in the document.
     Safe,
+
+    /// The SERVER safe mode level disallows the document from setting attributes that
+    /// would affect conversion of the document. This level trims docfile to its relative
+    /// path and prevents the document from:
+    ///
+    /// - setting source-highlighter, doctype, docinfo and backend
+    /// - seeing docdir (as it can reveal information about the host filesystem)
+    ///
+    /// It allows icons and linkcss. No includes from a url are allowed unless the
+    /// `allow-uri-read` attribute is set.
     Server,
+
+    /// The SECURE safe mode level disallows the document from attempting to read files
+    /// from the file system and including their contents into the document. Additionally,
+    /// it:
+    ///
+    /// - disables icons
+    /// - disables include directives (include::[])
+    /// - data can not be retrieved from URIs
+    /// - prevents access to stylesheets and JavaScript files
+    /// - sets the backend to html5
+    /// - disables docinfo files
+    /// - disables data-uri
+    /// - disables interactive (opts=interactive) and inline (opts=inline) modes for SVGs
+    /// - disables docdir and docfile (as these can reveal information about the host
+    ///   filesystem)
+    /// - disables source highlighting
+    ///
+    /// Note: GitHub processes AsciiDoc files using the SECURE mode.
     Secure,
 }
 
+/// Source of the document to parse
+///
+/// We support three sources:
+/// 1. Files: A list of file paths to read the document from
+/// 2. String: A string containing the document content
+/// 3. Stdin: Read the document content from standard input
 #[derive(Debug, Default, Clone, PartialEq)]
 pub enum Source {
+    /// A list of file paths to read the document from
     Files(Vec<PathBuf>),
+    /// A string containing the document content
     String(String),
+    /// Read the document content from standard input
     #[default]
     Stdin,
 }
