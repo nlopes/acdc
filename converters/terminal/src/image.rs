58502f3cef4dd1a911e54c75d80020cc7f0d5a00
Norberto Lopes <nlopes.ml@gmail.com>
2025-12-05 16:15:10 +0100

    fix(terminal): use images feature to improve compile times

--- converters/terminal/src/image.rs
+++ converters/terminal/src/image.rs
@@ -17,12 +17,23 @@ pub(crate) fn visit_image<V: WritableVisitor<Error = Error>>(
             w.queue(PrintStyledContent(format!("[Image: {url}]").italic()))?;
         }
         Source::Path(path) => {
-            let conf = viuer::Config::default();
-            viuer::print_from_file(path, &conf).unwrap_or_else(|e| {
-                tracing::warn!(?path, ?e, "Failed to display image");
-                (0, 0)
-            });
-            w.flush()?;
+            #[cfg(feature = "images")]
+            {
+                // Keep this flush else the image will render before any text in the
+                // buffer.
+                w.flush()?;
+                let conf = viuer::Config::default();
+                viuer::print_from_file(path, &conf).unwrap_or_else(|e| {
+                    tracing::warn!(?path, ?e, "Failed to display image");
+                    (0, 0)
+                });
+                w.flush()?;
+            }
+            #[cfg(not(feature = "images"))]
+            {
+                let display = path.display();
+                w.queue(PrintStyledContent(format!("[Image: {display}]").italic()))?;
+            }
         }
         Source::Name(name) => {
             w.queue(PrintStyledContent(format!("[Image: {name}]").italic()))?;
