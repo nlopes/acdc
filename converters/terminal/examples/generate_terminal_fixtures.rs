444991267e067ca4e79799f501ed80e59efd7200
Norberto Lopes <nlopes.ml@gmail.com>
2026-03-07 20:17:56 +0100

    chore: simplify fixture generation - better abstraction

--- converters/terminal/examples/generate_terminal_fixtures.rs
+++ converters/terminal/examples/generate_terminal_fixtures.rs
@@ -8,7 +8,7 @@ use acdc_converters_dev::generate_fixtures::FixtureGenerator;
 use acdc_converters_terminal::Processor;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    FixtureGenerator::new("terminal", "txt").generate(|doc, output| {
+    FixtureGenerator::new("terminal", "txt").generate(|_subdir, doc, output| {
         let processor =
             Processor::new(Options::default(), doc.attributes.clone()).with_terminal_width(80);
         processor.write_to(doc, output, None)?;
